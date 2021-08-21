d59add8a3f8121678ca2550bf4d3a1fd27f93351
andrey.oshkin <vchora96@yandex.ru>
2021-08-21 17:59:27 +0300

    фикс1

--- src/ru/oshkin/MaxNumber.java
+++ src/ru/oshkin/MaxNumber.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class MaxNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите целое число");
+        System.out.println("Введите целое число:");
         int a = scanner.nextInt();
-        System.out.println("Введите целое число");
+        System.out.println("Введите целое число:");
         int b = scanner.nextInt();
         if (a > b) {
             System.out.println("Большее число =" + a);
